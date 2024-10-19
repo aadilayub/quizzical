b0f23efbd2c4bdcb95730f451298bac1972cd876
aadilayub15 <aadilayub15@gmail.com>
2024-10-19 07:22:26 +0000

    formatting

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,37 +1,39 @@
-import { decode } from "html-entities";
+import {decode} from 'html-entities'
 
-function Question({ data, showAnswer, handleClick }) {
+function Question({data, showAnswer, handleClick}) {
   const incorrectAnswers = data.incorrect_answers.map((answer) => ({
     content: answer,
-    correct: false,
+    correct: false
   }))
 
   const answers = [
-    { content: data.correct_answer, correct: true },
-    ...incorrectAnswers,
-  ];
+    {content: data.correct_answer, correct: true},
+    ...incorrectAnswers
+  ]
 
-  answers.sort(() => Math.random() - 0.5); // shuffling the array
+  answers.sort(() => Math.random() - 0.5) // shuffling the array
 
   return (
     <div className="question-wrapper">
       <h2 className="question">{decode(data.question)}</h2>
       <ul className="answers-list">
         {answers.map((answer) => (
-          <li 
-            key={crypto.randomUUID()} 
-            className={`answer-list--item ${showAnswer ? (answer.correct ? 'correct' : 'wrong') : ''}`}
+          <li
+            key={crypto.randomUUID()}
+            className={`answer-list--item ${
+              showAnswer ? (answer.correct ? 'correct' : 'wrong') : ''
+            }`}
             onClick={(e) => {
               e.currentTarget.classList.toggle('selected')
-              handleClick(decode(answer.content))}
-            }
+              handleClick(decode(answer.content))
+            }}
           >
             {decode(answer.content)}
           </li>
         ))}
       </ul>
     </div>
-  );
+  )
 }
 
-export default Question;
+export default Question
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,24 +1,24 @@
-import Question from "./Question";
-import React from "react";
+import Question from './Question'
+import React from 'react'
 
 function Quiz() {
-  const [questions, setQuestions] = React.useState([]);
+  const [questions, setQuestions] = React.useState([])
   const [checked, setChecked] = React.useState(false)
   const [score, setScore] = React.useState(0)
 
   React.useEffect(() => {
     async function fetchData() {
       const res = await fetch(
-        "https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple"
-      );
+        'https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple'
+      )
       const data = await res.json()
       const questionData = data.results
       console.log(questionData)
       setQuestions(data.results)
     }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   function checkQuestion(selected, question) {
     if (selected == question.correct_answer) {
@@ -27,22 +27,31 @@ function Quiz() {
   }
 
   return (
-    <div className="quiz-screen" >
+    <div className="quiz-screen">
       <div className="questions-list">
         {questions.map((questionData) => (
-          <Question 
-            key={crypto.randomUUID()} 
-            data={questionData} 
+          <Question
+            key={crypto.randomUUID()}
+            data={questionData}
             showAnswer={checked}
-            handleClick={(selected) => { checkQuestion(selected, questionData) }}
+            handleClick={(selected) => {
+              checkQuestion(selected, questionData)
+            }}
             selectedAnswer={3}
           />
         ))}
       </div>
       {checked && <p>You scored {score}/5 correct answers</p>}
-      <div className="btn" onClick={() => {setChecked(true)}}>Check answers</div>
-    </div>  
-  );
+      <div
+        className="btn"
+        onClick={() => {
+          setChecked(true)
+        }}
+      >
+        Check answers
+      </div>
+    </div>
+  )
 }
 
-export default Quiz;
+export default Quiz
